3c97b6ce2febcd75f5b96c0a22808fd40920082d
Hifa <mousou.3@buckeyemail.osu.edu>
2021-07-15 19:41:45 -0400

    finished basic contact us page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  resources :contacts
   resources :users , only: [:new, :create]
   get 'login', to: 'sessions#new'
   post 'login', to:'sessions#create'
   get 'welcome',to:'sessions#welcome'
   get 'signup', to: 'users#new'
+  get 'contact', to: 'contacts#new'
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root :to => 'sessions#welcome'
